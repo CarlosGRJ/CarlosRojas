b94e23e7725f86733c8e48abbd53463bdf3526fa
CarlosGRJ <carlosgrj@outlook.com>
2021-05-29 10:00:26 -0500

    Cambios About

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,6 +7,7 @@ import { IconContactMe } from './IconContactMe';
 import { useForm } from '../../hooks/useForm';
 
 import classes from './Contact.module.scss';
+import Card from '../../components/UI/Card/Card';
 
 export const Contact = () => {
    const [formIsValid, setFormIsValid] = useState(false);
@@ -21,16 +22,8 @@ export const Contact = () => {
       const emailPattern = new RegExp(
          /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i,
       );
-      const identifier = setTimeout(() => {
-         console.log('Checking form validity!');
-         // email.includes('@')
-         setFormIsValid(emailPattern.test(email) && name && message);
-      }, 500);
-
-      return () => {
-         console.log('CLEANUP');
-         clearTimeout(identifier);
-      };
+      // email.includes('@')
+      setFormIsValid(emailPattern.test(email) && name && message);
    }, [email, name, message]);
 
    const handleSubmit = async (e) => {
@@ -58,67 +51,69 @@ export const Contact = () => {
    return (
       <div className={`${classes.contact} animate__animated animate__fadeIn`}>
          {/* <h1>Contact</h1> */}
-         <div className={classes['contact-box']}>
-            <div className={classes.contact__left}>
-               <IconContactMe classes={classes} />
-            </div>
-            <div className={classes.contact__right}>
-               <h2>Contact Me</h2>
+         <Card className={classes.card}>
+            {/* <div className={classes['contact-box']}> */}
+               <div className={classes.contact__left}>
+                  <IconContactMe classes={classes} />
+               </div>
+               <div className={classes.contact__right}>
+                  <h2>Contact Me</h2>
 
-               <form onSubmit={handleSubmit}>
-                  <div className={classes.form__group}>
-                     <input
-                        type='text'
-                        placeholder='Name'
-                        className={classes.form__input}
-                        id='name'
-                        autoComplete='off'
-                        name='name'
-                        value={name}
-                        onChange={handleInputChange}
-                     />
-                     <label hmtlfor='name' className={classes.form__label}>
-                        Name
-                     </label>
-                  </div>
+                  <form onSubmit={handleSubmit}>
+                     <div className={classes.form__group}>
+                        <input
+                           type='text'
+                           placeholder='Name'
+                           className={classes.form__input}
+                           id='name'
+                           autoComplete='off'
+                           name='name'
+                           value={name}
+                           onChange={handleInputChange}
+                        />
+                        <label hmtlfor='name' className={classes.form__label}>
+                           Name
+                        </label>
+                     </div>
 
-                  <div className={classes.form__group}>
-                     <input
-                        type='email'
-                        className={classes.form__input}
-                        placeholder='Email'
-                        id='email'
-                        required
-                        autoComplete='off'
-                        name='email'
-                        value={email}
-                        onChange={handleInputChange}
-                     />
-                     <label hmtlfor='email' className={classes.form__label}>
-                        Email
-                     </label>
-                  </div>
+                     <div className={classes.form__group}>
+                        <input
+                           type='email'
+                           className={classes.form__input}
+                           placeholder='Email'
+                           id='email'
+                           required
+                           autoComplete='off'
+                           name='email'
+                           value={email}
+                           onChange={handleInputChange}
+                        />
+                        <label hmtlfor='email' className={classes.form__label}>
+                           Email
+                        </label>
+                     </div>
 
-                  <div className={classes.form__group}>
-                     <textarea
-                        placeholder='Message'
-                        className={classes.form__input}
-                        name='message'
-                        value={message}
-                        onChange={handleInputChange}></textarea>
-                  </div>
+                     <div className={classes.form__group}>
+                        <textarea
+                           placeholder='Message'
+                           className={classes.form__input}
+                           name='message'
+                           value={message}
+                           onChange={handleInputChange}></textarea>
+                     </div>
 
-                  {/* <div> */}
-                  <Button disabled={!formIsValid} type='submit'>
-                     Send
-                  </Button>
-                  {/* <button disabled={!formIsValid} type='submit'>
+                     <div>
+                        <Button className={classes.form__button} disabled={!formIsValid} type='submit'>
+                           Send
+                        </Button>
+                        {/* <button disabled={!formIsValid} type='submit'>
                      Send
                   </button> */}
-                  {/* </div> */}
-               </form>
-            </div>
-         </div>
+                     </div>
+                  </form>
+               </div>
+            {/* </div> */}
+         </Card>
       </div>
    );
 };
